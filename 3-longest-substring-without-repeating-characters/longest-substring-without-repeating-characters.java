12f8b77447943c4263bb4a6ed653354e81b0dd52
Ayush <151222788+AyushChauhan2004@users.noreply.github.com>
2026-09-26 01:03:04 +0530

    Time: 65 ms (49.10%) | Memory: 47.9 MB (39.72%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,5 +1,6 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        // SO first created a set for storing then then used sliding window basic concept for checking occurances as soon as we find that the element is already present in the set then we remove from left and add element from right and then return length or whatever we are requried to give
         Set<Character> set1 = new HashSet<>();
          int l = 0;
          int maxlen = 0;
